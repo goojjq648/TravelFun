fec4b73da6d01186dccd047f39e0bdf2aed3b03f
goojjq648 <goojjq648@gmail.com>
2024-07-25 22:21:27 +0800

    feat : 新增專案檔案、設定檔案

---
+++ TravelFun/food_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def food_mainPage(request):
+    return render(request, 'food_app/food.html')
---
+++ TravelFun/main_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'main_app/home.html')
